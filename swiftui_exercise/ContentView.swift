1d4dc766a7d289cfc7bff5f761f048f99595a345
kfahmi77 <khoirulfahmi44@gmail.com>
2024-06-30 20:03:57 +0700

    Initial Commit

---
+++ swiftui_exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swiftui_exercise
+//
+//  Created by Khoirul Fahmi on 30/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftui_exercise/swiftui_exerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_exerciseApp.swift
+//  swiftui_exercise
+//
+//  Created by Khoirul Fahmi on 30/06/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_exerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
