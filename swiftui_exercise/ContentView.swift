1b09f7cc9d51c0f216791fb0867aa4a49514ffe3
kfahmi77 <khoirulfahmi44@gmail.com>
2024-07-03 21:07:11 +0700

    craete card

--- swiftui_exercise/ContentView.swift
+++ swiftui_exercise/ContentView.swift
@@ -9,21 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Lorem Ipsum dol amet kwjdfjbjbfjsbjaifsvihisfvjbsridihfshuhfusjhjhjdfshjf")
-            .fontWeight(.bold)
-            .font(.system(size:20))
-            .foregroundColor(.green)
-            .multilineTextAlignment(.center)
-            .lineLimit(nil)
-            .lineSpacing(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
-            .padding()
-            .rotation3DEffect(
-                .degrees(60),axis: (x:1,y:0,z:0))
-            .shadow(color: .blue, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/,x: 0,y: 15)
-            .truncationMode(.middle)
+        VStack {
+            HeaderView()
+        }
+        HStack {
+            VStack{
+                Text("Basic")
+                    .font(.system(.title,design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                Text("Rp.800")
+                    .font(.system(size: 25,weight: .heavy,design: .rounded))
+                    .foregroundColor(.white)
+                Text("per bulan")
+                    .font(.headline)
+                    .foregroundColor(.white)
+            }
+            .frame(minWidth: 0,maxWidth: .infinity,minHeight:100)
+            .padding(40)
+            .background(Color.gray)
+            .cornerRadius(10)
+            
+            VStack{
+                Text("Pro")
+                    .font(.system(.title,design: .rounded))
+                    .fontWeight(.black)
+                    .foregroundColor(.white)
+                Text("Rp.1k")
+                    .font(.system(size: 25,weight: .heavy,design: .rounded))
+                    .foregroundColor(.white)
+                Text("per bulan")
+                    .font(.headline)
+                    .foregroundColor(.white)
+            }
+            .frame(minWidth: 0,maxWidth: .infinity,minHeight:100)
+            .padding(40)
+            .background(Color.purple)
+            .cornerRadius(10)
+        }
+        .padding(.horizontal)
+        
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct HeaderView: View {
+    var body: some View {
+        VStack(alignment:.leading,spacing: 2) {
+            Text("Pilih")
+                .font(.system(.largeTitle,design: .rounded))
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            Text("Paketmu")
+                .font(.system(.largeTitle,design: .rounded))
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        }
+    }
 }
