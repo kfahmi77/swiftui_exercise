c97dd90e60f66a31456e62daacd656c10007fa5a
kfahmi77 <khoirulfahmi44@gmail.com>
2024-07-04 19:33:42 +0700

    refactor code card plan

--- swiftui_exercise/ContentView.swift
+++ swiftui_exercise/ContentView.swift
@@ -12,43 +12,34 @@ struct ContentView: View {
         VStack {
             HeaderView()
         }
-        HStack {
-            VStack{
-                Text("Basic")
-                    .font(.system(.title,design: .rounded))
-                    .fontWeight(.black)
+        HStack(spacing:15) {
+            CardPlan(title: "Basic", price: "200", textColor: .white, bgColor: .blue)
+            ZStack {
+                CardPlan(title: "Pro", price: "400", textColor: .white, bgColor: .purple)
+                
+                Text("Best for Beginner")
+                    .font(.system(.caption,design: .rounded))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .foregroundColor(.white)
-                Text("Rp.800")
-                    .font(.system(size: 25,weight: .heavy,design: .rounded))
-                    .foregroundColor(.white)
-                Text("per bulan")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(.black)
+                    .offset(x:0,y: 87)
             }
-            .frame(minWidth: 0,maxWidth: .infinity,minHeight:100)
-            .padding(40)
-            .background(Color.gray)
-            .cornerRadius(10)
-            
-            VStack{
-                Text("Pro")
-                    .font(.system(.title,design: .rounded))
-                    .fontWeight(.black)
+        }
+        .padding(15)
+        VStack(spacing:15){
+            ZStack {
+                CardPlan(title: "Team", price: "1200", textColor: .white, bgColor: .green)
+                Text("Best for Team with 20 member")
+                    .font(.system(.caption,design: .rounded))
+                    .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                     .foregroundColor(.white)
-                Text("Rp.1k")
-                    .font(.system(size: 25,weight: .heavy,design: .rounded))
-                    .foregroundColor(.white)
-                Text("per bulan")
-                    .font(.headline)
-                    .foregroundColor(.white)
+                    .padding(5)
+                    .background(.black)
+                    .offset(x:0,y: 87)
             }
-            .frame(minWidth: 0,maxWidth: .infinity,minHeight:100)
-            .padding(40)
-            .background(Color.purple)
-            .cornerRadius(10)
         }
         .padding(.horizontal)
-        
     }
 }
 
@@ -62,9 +53,36 @@ struct HeaderView: View {
             Text("Pilih")
                 .font(.system(.largeTitle,design: .rounded))
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
             Text("Paketmu")
                 .font(.system(.largeTitle,design: .rounded))
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
         }
+    }
+}
+
+struct CardPlan: View {
+    var title : String
+    var price : String
+    var textColor : Color
+    var bgColor : Color
+    
+    var body: some View {
+        VStack{
+            Text(title)
+                .font(.system(.title,design: .rounded))
+                .fontWeight(.black)
+                .foregroundColor(.white)
+            Text(price)
+                .font(.system(size: 25,weight: .heavy,design: .rounded))
+                .foregroundColor(textColor)
+            Text("per bulan")
+                .font(.headline)
+                .foregroundColor(.white)
+        }
+        .frame(minWidth: 0,maxWidth: .infinity,minHeight:100)
+        .padding(40)
+        .background(bgColor)
+        .cornerRadius(10)
     }
 }
